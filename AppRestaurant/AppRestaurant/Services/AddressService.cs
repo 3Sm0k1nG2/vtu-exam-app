984dd81aa75e82ef6ec8324b56b27c47fefab16f
3Sm0k1nG2 <3sm0k1ng2@Gmail.com>
2022-06-20 00:22:15 +0300

    Implemented service for addresses.

---
+++ AppRestaurant/AppRestaurant/Services/AddressService.cs
@@ -0,0 +1,18 @@
+﻿using AppRestaurant.Models;
+using AppRestaurant.Models.Forms;
+using AppRestaurant.Data;
+
+namespace AppRestaurant.Services
+{
+    public class AddressService
+    {
+        public int GetLastID()
+        {
+            return AddressDb.lastId;
+        }
+        public void Create(AddressFormDataModel formModel)
+        {
+            new AddressDb().Create(formModel);
+        }
+    }
+}
